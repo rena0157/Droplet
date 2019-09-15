3e4e063f6799869de5ccd874dfaeb408decfdf09
Adam Renaud <adamf.renaud@gmail.com>
2019-09-15 16:37:47 -0400

    Refactoring the double option into its own class

---
+++ src/Droplet.Core.Inp/Options/InpDoubleOption.cs
@@ -0,0 +1,30 @@
+﻿using Droplet.Core.Inp.Data;
+using Droplet.Core.Inp.Exceptions;
+
+namespace Droplet.Core.Inp.Options
+{
+    /// <summary>
+    /// Implementation for <see cref="InpOption{double}"/> that holds
+    /// common code for this class
+    /// </summary>
+    public class InpDoubleOption : InpOption<double>
+    {
+        /// <summary>
+        /// Constructor that sets the value from the <paramref name="row"/> data
+        /// and sets the database that the option belongs to
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="database"></param>
+        public InpDoubleOption(IInpTableRow row, IInpDatabase database) : base(row, database)
+        {
+            // Try to parse the row
+            if (double.TryParse(row[1], out var slope))
+                // If it suceeds then assign the value of the parsing
+                Value = slope;
+            // If it is not sucessful throw an new exception
+            else
+                throw new InpParseException($"The parsing of {this} was unsucessful due to a double" +
+                    $" conversion issue");
+        }
+    }
+}
--- src/Droplet.Core.Inp/Options/MinSlopeOption.cs
+++ src/Droplet.Core.Inp/Options/MinSlopeOption.cs
@@ -6,7 +6,7 @@ namespace Droplet.Core.Inp.Options
     /// <summary>
     /// The minimum slope option
     /// </summary>
-    public class MinSlopeOption : InpOption<double>
+    public class MinSlopeOption : InpDoubleOption
     {
         /// <summary>
         /// Default Constructor that initializes the option from a row
@@ -16,14 +16,6 @@ namespace Droplet.Core.Inp.Options
         /// <param name="database">The database that this option belongs to</param>
         public MinSlopeOption(IInpTableRow row, IInpDatabase database) : base(row, database)
         {
-            // Try to parse the row
-            if (double.TryParse(row[1], out var slope))
-                // If it suceeds then assign the value of the parsing
-                Value = slope;
-            // If it is not sucessful throw an new exception
-            else
-                throw new InpParseException($"The parsing of {this} was unsucessful due to a double" +
-                    $" conversion issue");
         }
 
         /// <summary>
