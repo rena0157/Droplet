f056ceb661d569976587c363c04e10fc955cc019
Adam Renaud <adamf.renaud@gmail.com>
2019-09-22 09:45:55 -0400

    Added the InpTimeSpanOptionTests class for testing TimeSpan Options

---
+++ tests/Droplet.Core.Inp.Tests/Options/InpTimeSpanOptionTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace Droplet.Core.Inp.Tests.Options
+{
+    public class InpTimeSpanOptionTests : FileTestsBase
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="logger">A logger that is passed from XUnit</param>
+        public InpTimeSpanOptionTests(ITestOutputHelper logger) : base(logger)
+        {
+        }
+
+        #endregion
+
+        #region Dry Days Option
+
+
+
+        #endregion
+    }
+}
