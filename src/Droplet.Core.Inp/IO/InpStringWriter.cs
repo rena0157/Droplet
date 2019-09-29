1d534e52ea73bcce636ed4ae35f7e5dc7df8feff
Adam Renaud <adamf.renaud@gmail.com>
2019-09-29 12:01:04 -0400

    Added a helper class, might remove in the future

--- src/Droplet.Core.Inp/IO/InpStringWriter.cs
+++ src/Droplet.Core.Inp/IO/InpStringWriter.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Droplet.Core.Inp.Data;
+using Droplet.Core.Inp.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Droplet.Core.Inp.IO
 {
@@ -11,5 +14,21 @@ namespace Droplet.Core.Inp.IO
     public class InpStringWriter
     {
 
+        private readonly IInpDatabase _database;
+
+        public InpStringWriter(IInpDatabase database)
+        {
+            _database = database;
+        }
+
+        /// <summary>
+        /// Convert the database to strings where each string is 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<string> DatabaseToStrings()
+        {
+            foreach(var entity in _database.GetAllEntities<InpEntity>())
+                yield return entity.ToInpString();
+        }
     }
 }
