2d8bf310eebfec4a4d1f0374746d296825eff608
Adam Renaud <adamf.renaud@gmail.com>
2019-10-20 15:09:54 -0400

    Working on Host Builder

---
+++ src/Droplet.WindowsApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Droplet.WindowsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<App>();
+                });
+    }
+}
---
+++ src/Droplet.WindowsAppC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Droplet.WindowsAppC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
