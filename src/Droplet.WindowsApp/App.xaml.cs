13866027ff27688a9ae07078bbd943067323683b
Adam Renaud <adamf.renaud@gmail.com>
2019-10-26 12:00:34 -0400

    Adding basic host setup

--- src/Droplet.WindowsApp/App.xaml.cs
+++ src/Droplet.WindowsApp/App.xaml.cs
@@ -1,17 +1,134 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace Droplet.WindowsApp
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// The Application class for the windows project
     /// </summary>
     public partial class App : Application
     {
+
+        #region Private Members
+
+        /// <summary>
+        /// The default host provider
+        /// </summary>
+        private IHost _host;
+
+        /// <summary>
+        /// The <see cref="App"/> class logger
+        /// </summary>
+        private ILogger<App> _logger;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor that will build the host and required services
+        /// </summary>
+        public App()
+        {
+            // Create a console
+            AllocConsole();
+        }
+
+        #endregion
+
+        #region Host Builder Methods
+
+        /// <summary>
+        /// Allocates a console to the application
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
+
+        /// <summary>
+        /// The default host builder application
+        /// </summary>
+        /// <returns></returns>
+        public static IHostBuilder CreateHostBuilder(string[] args)
+            => Host.CreateDefaultBuilder(args)
+            .ConfigureHostConfiguration((configHost) =>
+            {
+
+            })
+            .ConfigureAppConfiguration((configApp) =>
+            {
+
+            })
+            .ConfigureServices((context, services) =>
+            {
+
+            })
+            .UseConsoleLifetime((config) => 
+            {
+                config.SuppressStatusMessages = true;
+            });
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Default service provider for the application
+        /// </summary>
+        public IServiceProvider ServiceProvider { get; private set; }
+
+        /// <summary>
+        /// The default configuration for the application
+        /// </summary>
+        public IConfiguration Configuration { get; private set; }
+
+        #endregion
+
+        #region Startup and Shutdown Methods
+
+        /// <summary>
+        /// Application startup for WPF
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e">The startup Arguments</param>
+        private async void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Build the host
+            _host = CreateHostBuilder(e.Args).Build();
+            ServiceProvider = _host.Services;
+
+            await _host.StartAsync();
+
+            // Get required services
+            _logger = ServiceProvider.GetRequiredService<ILogger<App>>();
+            Configuration = ServiceProvider.GetRequiredService<IConfiguration>();
+
+            // Log that the application has started
+            _logger.LogInformation("Application Started");
+            foreach (var arg in e.Args)
+                _logger.LogInformation(arg);
+
+        }
+
+        private async void Application_Exit(object sender, ExitEventArgs e)
+        {
+            using (_host)
+            {
+                await _host.StopAsync(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        #endregion
     }
 }
